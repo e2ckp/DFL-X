aabe4168a6ca9c0cb87cd235cddbb98c18da9a04
= <=>
2019-08-16 15:19:10 +0800

    修改用户路径

--- src/path.ts
+++ src/path.ts
@@ -5,4 +5,4 @@ export const _rootPath = isDev ? './' : './resources/app.asar.unpacked/';
 //静态文件目录
 export const _static = `${_rootPath}static`;
 //配置文件目录
-export const _config = `${_rootPath}config`;
+export const _config = `${_rootPath}user-config`;
